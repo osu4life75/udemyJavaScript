9ec467d2b1879041932153de11668f2e158fe491
George Lynch <osu4life75@gmail.com>
2023-07-11 22:32:56 -0500

    Lessons
    Learning Loops

---
+++ script.js
@@ -0,0 +1,22 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+var todosLength = todos.length;
+//for ( var i=0; i < todosLength; i++ ) {
+//     todos.pop();
+//}
+
+//var counterOne = 10;
+//while (counterOne > 0) {
+ //   console.log(counterOne);
+  //  counterOne--
+//}
+//var counterTwo = 10;
+//do {
+   // console.log(counterTwo)
+   // counterTwo++
+//}while (counterTwo > 0);
