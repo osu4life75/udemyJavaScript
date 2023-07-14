709bfa6c3455c2832dbd728dd28409674ec62749
George Lynch <osu4life75@gmail.com>
2023-07-13 21:40:23 -0500

    JS
    Testing JavaScript functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Loops
+
 var todos = [
     "clean room",
     "brush teeth",
@@ -20,3 +22,22 @@ var todosLength = todos.length;
    // console.log(counterTwo)
    // counterTwo++
 //}while (counterTwo > 0);
+
+
+//if (name === "Billy" || name === "Ann"){
+  //alert("Hi BIlly or Ann!");
+//}
+
+// Get the button element
+const button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+  // Perform your desired action when the button is clicked
+  console.log('Button clicked!');
+});
+
+function multiply(a, b) {
+ return a * b
+}
+multiply(5, 10);
