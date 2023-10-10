258d9ff5d9d2e6d45f1e98e1a9c097a6460371cf
George Lynch <osu4life75@gmail.com>
2023-10-10 17:02:42 -0500

    Practice Work

--- Server/server.js
+++ Server/server.js
@@ -4,17 +4,17 @@ const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
-// Create a MySQL database connection
-// const db = mysql.createConnection({
-  // host: 'localhost',
-  // user: 'your_db_user',
-  // password: 'your_db_password',
-  // // database: 'your_database_name',
+ //Create a MySQL database connection
+ //const db = mysql.createConnection({
+//host: 'localhost',
+//user: 'your_db_user',
+   //password: 'your_db_password',
+   //database: 'your_database_name',
 // });
 
-// Define a route to handle form submissions
-// app.post('/submit', (req, res) => {
-//   const { firstName, lastName, height, weight, highSchool, gpa, position, otherPositions } = req.body;
+//Define a route to handle form submissions
+//app.post('/submit', (req, res) => {
+//const { firstName, lastName, height, weight, highSchool, gpa, position, otherPositions } = req.body;
 
 //   // Insert the form data into the database
 //   const sql = 'INSERT INTO registration (first_name, last_name, height, weight, high_school, gpa, position, other_positions) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-const submit = document.getElementById('submit');
-submit.addEventListener('click', () => {
-  const formData = {
-    firstName: document.querySelector('[aria-label="First name"]').value,
-    lastName: document.querySelector('[aria-label="Last name"]').value,
-    height: document.querySelector('[aria-label="Height"]').value,
-    weight: document.querySelector('[aria-label="Weight"]').value,
-    highSchool: document.querySelector('[aria-label="High School"]').value,
-    gpa: document.querySelector('[aria-label="GPA"]').value,
-    position: document.querySelector('[aria-label="Position"]').value,
-    otherPositions: document.querySelector('[aria-label="Other Postions Played"]').value,
-  };
+let button = document.getElementById('submit');
+button.addEventListener('click', function submitButton(params) {
+   console.log('submitButton');
+})
 
-  // Send a POST request to the server
-  fetch('/submit', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => {
-      if (response.ok) {
-        alert('Form Submitted Successfully');
-      } else {
-        alert('Error Submitting Form');
-      }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      alert('Error Submitting Form');
-    });
-});
